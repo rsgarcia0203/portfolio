beb021e1de78dcc4b247c19804bd6d86bd0d8c9c
Ronny Garcia <rsgarcia@espol.edu.ec>
2023-11-06 17:05:34 -0500

    Crear componente
    Se procede a crear un componente para el manejo de links de la barra de navegación.

---
+++ src/app/components/nav-link/nav-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-link',
+  templateUrl: './nav-link.component.html',
+  styleUrls: ['./nav-link.component.css']
+})
+export class NavLinkComponent {
+
+}
