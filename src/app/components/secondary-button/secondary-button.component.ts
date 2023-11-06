25314a08543cb78765632e87996eee4827e90a06
Ronny Garcia <rsgarcia@espol.edu.ec>
2023-11-06 18:36:08 -0500

    Crear Componente
    Se procede a crear un componente que servirá de botón secundario para la aplicación.

---
+++ src/app/components/secondary-button/secondary-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-button',
+  templateUrl: './secondary-button.component.html',
+  styleUrls: ['./secondary-button.component.css']
+})
+export class SecondaryButtonComponent {
+
+}
